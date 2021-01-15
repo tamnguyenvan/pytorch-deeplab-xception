9246cda01e2ec72ec22622793e20f07db7c18638
Nguyễn Văn Tâm <tamnguyenvan@users.noreply.github.com>
2021-01-15 16:58:34 +0700

    Updated

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,4 +1,4 @@
-from dataloaders.datasets import cityscapes, coco, combine_dbs, pascal, sbd
+from dataloaders.datasets import cityscapes, coco, combine_dbs, pascal, sbd, nyuv2
 from torch.utils.data import DataLoader
 
 def make_data_loader(args, **kwargs):
@@ -36,7 +36,16 @@ def make_data_loader(args, **kwargs):
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
         test_loader = None
         return train_loader, val_loader, test_loader, num_class
+    elif args.dataset == 'nyuv2':
+        train_set = nyuv2.NYUv2Segmentation(args, split='train')
+        val_set = nyuv2.NYUv2Segmentation(args, split='val')
 
+        num_class = train_set.NUM_CLASSES
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
+        test_loader = None
+
+        return train_loader, val_loader, test_loader, num_class
     else:
         raise NotImplementedError
 
