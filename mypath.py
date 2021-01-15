cf7f925da2749b7dc7b6204ac4da8581f0d82777
Nguyễn Văn Tâm <tamnguyenvan@users.noreply.github.com>
2021-01-15 18:34:11 +0700

    Added dataset path

--- mypath.py
+++ mypath.py
@@ -9,6 +9,8 @@ class Path(object):
             return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
             return '/path/to/datasets/coco/'
+        elif dataset == 'nyuv2':
+            return '/content/pytorch-deeplab-xception/data
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
